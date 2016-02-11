6cdc4915ad77e5eed2ca630f327ebf01c899faac
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-11 10:29:26 +0200

    add user

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -9,12 +9,3 @@ class SomeTests(TestCase):
         put docstrings in your tests
         """
         assert(2 + 2 == 4)
-
-        user = User(
-                        username='sanya',
-                        is_active=True,
-                        is_superuser=True,
-                        is_staff=True,
-                        email='sanya071186@gmail.com')
-        user.set_password('11')
-        user.save()
