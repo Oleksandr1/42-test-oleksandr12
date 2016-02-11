81ce88c38a909e21ea6749614b76abf59c766c62
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-11 10:53:50 +0200

    flake8correction

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from models import MyData
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -15,7 +15,6 @@ def main_page(request):
     user.set_password('11')
     user.save()
     """
-
-    
+        
     my_data = MyData.objects.first
     return render(request, 'hello/main.html', {'my_data': my_data})
