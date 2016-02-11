d6d9d1ca151c9d6c700d5dc19f6ead3f74055384
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-11 11:46:30 +0200

    add static

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-
+from fortytwo_test_task.settings import STATIC_URL, STATIC_ROOT
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,4 +13,4 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'hello.views.main_page', name='home'),
 )
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
