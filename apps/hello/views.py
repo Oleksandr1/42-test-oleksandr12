a551a0eb07978986f6c3b272d289b139bab00444
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-16 14:28:35 +0200

    add navigation

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,27 +1,23 @@
 from django.shortcuts import render
 from models import MyData, RequestHistory
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
 def main_page(request):
-    """
-    user = User(
-                    username='sanya',
-                    is_active=True,
-                    is_superuser=True,
-                    is_staff=True,
-                    email='sanya071186@gmail.com')
-    user.set_password('11')
-    user.save()
-    """
     my_data = MyData.objects.first
     return render(request, 'hello/main.html', {'my_data': my_data})
 
 
 def requests(request):
     req = list(reversed(RequestHistory.objects.all()))
-    pag = Paginator(req, 10)
-    page_1 = pag.page(1)
+    paginator = Paginator(req, 10)
+    page = request.GET.get('page')
+    try:
+        req=paginator.page(page)
+    except PageNotAnInteger:
+        req=paginator.page(1)
+    except EmptyPage:
+        req=paginator.page(paginator.num_pages)
     return render(request, 'hello/requests.html',
-                  {'requests': page_1.object_list})
+                  {'requests': req})
